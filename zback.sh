df60254e34480090499746f8f1d2db1480e0fc6a
miz <miz@about.me>
2017-10-23 16:34:43 +0900

    差分バックアップ対応

--- zback.sh
+++ zback.sh
@@ -4,10 +4,11 @@
 #  zback.sh  : 定義ファイルを基にバックアップを行う。
 #-----------------------------------------------------------------------------------
 #  author    : miz <miz@about.me>
-#  version   : 1.2
+#  version   : 1.3
 #  todo      : なし
 #  
 #  ChangeLog : 
+#   ver 1.3    2017/10/20(金)  差分バックアップ対応
 #   ver 1.2    2017/08/21(月)  UTF8化。
 #   ver 1.1    2014/04/02(月)  若干、整形。
 #   ver 1.0    2009/04/06(月)  新規作成
@@ -33,13 +34,14 @@ main()
 	#--------- Option Parse
 	is_pass=$FALSE
 	suffix=""
-	while getopts 'dl' OPTION
+	while getopts 'dlu:' OPTION
 	do
 		case $OPTION in
 			\?) OPT_ERROR=1; break;;
 			#--------- def ファイル一覧を表示して終了
 			"d" ) suffix="_$bk_date";;
 			"l" ) ls -l ${def_path}/*.def && exit 0;;
+			"u" ) basedate="$OPTARG" ;;
 		esac
 	done
 	shift $(expr $OPTIND - 1)
@@ -49,6 +51,10 @@ main()
 	fi
 
 	dir_check
+
+	if [ "$basedate" != "" ]; then
+		basedate_check
+	fi
 
 	cd ${lnk_path}
 
@@ -67,10 +73,36 @@ function dir_check() {
 	test ! -d ${bak_path} && echo -e "Not Found. ${bak_path}" && exit 1
 }
 
+function basedate_check() {
+
+	expr "$basedate" + 1 >/dev/null 2>&1
+	if [ $? -eq 2 ]    #not Numeric
+	then
+		echo "  -u 日付 の指定に誤りがあります。{${basedate}}"
+		optstr=""
+		exit 1
+	fi
+
+	if [ $basedate -ge 20000101 ]; then
+		optstr=" --newer-mtime=${basedate}"
+	elif [ $basedate -lt 1000 ]; then
+		optstr=" --newer-mtime \"${basedate} days ago\""
+	else
+		echo "  -u 日付 の指定に誤りがあります。{${basedate}}"
+		optstr=""
+		exit 1
+	fi
+}
+
 function exec_backup() {
 
+	echo "archive option ： ${optstr}"
 	out_file=${bak_path}/${infile}${suffix}.tar.gz
-	${tarcmd} cfhz ${out_file} -T ${def_path}/${infile}.def
+	execcmd="${tarcmd} cfhz ${out_file} -T ${def_path}/${infile}.def ${optstr}"
+
+	eval "${execcmd}"
+	#sh -c "${execcmd}"    と等価
+
 	if [ $? -eq $SUCCESS ]; then
 		echo "  ${out_file}     を作成しました。"
 	else
@@ -80,7 +112,7 @@ function exec_backup() {
 }
 
 usage() {
-	echo -e "usage:`basename $0` [-d date, -l] DEF_FILES..."
+	echo -e "usage:`basename $0` [-d, -l, -u n|yyyymmdd] DEF_FILES..."
 	exit 1
 }
 
